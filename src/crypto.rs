cd4c310c1da8e1ef5e5ee5087db0f208715b7f0a
Greg Kapka <greg@oraclize.it>
2019-06-28 23:30:16 +0100

    chore(admin): factor fxns apart by renaming module

--- src/crypto.rs
+++ src/crypto.rs
@@ -10,51 +10,6 @@ use tiny_keccak::Keccak;
 
 type Result<T> = result::Result<T, AppError>;
 
-struct KeyStruct {
-    pub_vk: Option<HexKey>,
-    pub_sk: Option<HexKey>,
-    priv_vk: Option<HexKey>,
-    priv_sk: Option<HexKey>,
-}
-
-impl KeyStruct {
-    pub fn init_with_priv_sk(mut self, priv_sk: HexKey) -> Self {
-        self.priv_sk = Some(priv_sk);
-        self
-    }
-
-    pub fn add_priv_vk(mut self, priv_vk: HexKey) -> Self {
-        self.priv_vk = Some(priv_vk);
-        self
-    }
-
-    pub fn add_pub_sk(mut self, pub_sk: HexKey) -> Self {
-        self.pub_sk = Some(pub_sk);
-        self
-    }
-
-    pub fn add_pub_vk(mut self, pub_vk: HexKey) -> Self {
-        self.pub_vk = Some(pub_vk);
-        self
-    }
-
-    pub fn get_priv_sk(self) -> HexKey {
-        self.priv_sk.expect("No private spend key set in struct!")
-    }
-
-    pub fn get_priv_vk(self) -> HexKey {
-        self.priv_vk.expect("No private view key set in struct!")
-    }
-
-    pub fn get_pub_sk(self) -> HexKey {
-        self.pub_sk.expect("No public spend key set in struct!")
-    }
-
-    pub fn get_pub_vk(self) -> HexKey {
-        self.pub_vk.expect("No public view key set in struct!")
-    }
-}
-
 fn generate_256_bit_random_number() -> Result<BigUint> {
     Ok(rand::thread_rng().gen_biguint(256))
 }
@@ -82,7 +37,6 @@ fn convert_keccak256_hash_to_scalar_mod_order(hash: Keccak256Hash) -> Result<Sca
     convert_32_byte_arr_to_scalar_mod_order(hash)
 }
 
-// FIXME: Must we use this as opposed to Scalar::random?
 fn generate_random_scalar() -> Result<Scalar> {
     generate_256_bit_random_number()
         .and_then(convert_256_bit_big_uint_to_byte_arr)
