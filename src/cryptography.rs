d8676b417c48366e76848a48339dde7e175540b7
Greg Kapka <greg@oraclize.it>
2019-07-08 08:23:31 +0100

    feat(cryptog): adds helper fxns for monero key generation

--- src/cryptography.rs
+++ src/cryptography.rs
@@ -20,6 +20,10 @@ pub fn generate_random_scalar() -> Result<Scalar> {
     Ok(Scalar::random(&mut thread_rng()))
 }
 
+fn convert_key_to_scalar_mod_order(key: Key) -> Result<Scalar> {
+    Ok(Scalar::from_bytes_mod_order(key))
+}
+
 pub fn generate_random_scalar_mod_order() -> Result<Scalar> {
     generate_random_scalar().and_then(reduce_scalar_mod_l)
 }
@@ -36,21 +40,89 @@ pub fn keccak256_hash_bytes(bytes: &[u8]) -> Result<Keccak256Hash> {
     Ok(res)
 }
 
+pub fn convert_edwards_point_to_bytes(x: CompressedEdwardsY) -> Result<Key> {
+    Ok(x.to_bytes())
 }
 
+pub fn convert_scalar_to_bytes(x: Scalar) -> Result<Key> {
+    Ok(x.to_bytes())
 }
 
 fn compress_edwards_point(e_point: EdwardsPoint) -> Result<CompressedEdwardsY> {
     Ok(e_point.compress())
 }
 
-pub fn multiply_scalar_by_basepoint(scalar: Scalar) -> Result<Scalar> {
-    compress_edwards_point(scalar * ED25519_BASEPOINT_POINT)
-        .and_then(|x| Ok(convert_compressed_edwards_y_to_scalar(x)?))
+pub fn multiply_scalar_by_basepoint(scalar: Scalar) -> Result<CompressedEdwardsY> {
+    Ok(compress_edwards_point(&scalar * &ED25519_BASEPOINT_TABLE)?)
 }
 
 pub fn reduce_scalar_mod_l(scalar: Scalar) -> Result<Scalar> {
     Ok(scalar.reduce())
+}
+
+pub fn convert_32_byte_array_to_scalar(byte_arr: Key) -> Result<Scalar> {
+    match Scalar::from_canonical_bytes(byte_arr) {
+        Some(canonical_scalar) => Ok(canonical_scalar),
+        None => Err(
+            AppError::Custom("✘ Not a point on the edwards curve!".to_string())
+        )
+    }
+}
+
+pub fn convert_hex_string_to_scalar(priv_sk: String) -> Result<Scalar> {
+    convert_hex_string_to_32_byte_array(priv_sk)
+        .and_then(convert_32_byte_array_to_scalar)
+}
+
+pub fn generate_priv_vk_from_priv_sk(priv_sk: Scalar) -> Result<Scalar> {
+    keccak256_hash_bytes(&priv_sk.to_bytes())
+        .and_then(convert_keccak256_hash_to_scalar_mod_order)
+}
+
+pub fn concatenate_address(
+    keys: MoneroKeys,
+    prefix_byte: [u8; 1],
+    suffix_bytes: [u8; 4]
+) -> Result<Address> {
+    let pub_sk: Key = keys.get_pub_sk()?;
+    let pub_vk: Key = keys.get_pub_vk()?;
+    let mut address_bytes = [0; 69];
+    address_bytes[..1].copy_from_slice(&prefix_byte);
+    address_bytes[1..33].copy_from_slice(&pub_sk);
+    address_bytes[33..65].copy_from_slice(&pub_vk);
+    address_bytes[65..69].copy_from_slice(&suffix_bytes[..4]);
+    Ok(address_bytes)
+}
+
+pub fn hash_pub_keys_with_prefix(
+    keys: MoneroKeys,
+    prefix: [u8; 1]
+) -> Result<Keccak256Hash> {
+    let pub_sk: Key = keys.get_pub_sk()?;
+    let pub_vk: Key = keys.get_pub_vk()?;
+    let mut bytes_to_hash = vec![0; 65];
+    bytes_to_hash[..1].copy_from_slice(&prefix);
+    bytes_to_hash[1..33].copy_from_slice(&pub_sk);
+    bytes_to_hash[33..65].copy_from_slice(&pub_vk);
+    keccak256_hash_bytes(&bytes_to_hash)
+}
+
+pub fn get_address_suffix_from_hash(hash: Keccak256Hash) -> Result<[u8; 4]> {
+    let mut x = [0; 4];
+    x.copy_from_slice(&hash[..4]);
+    Ok(x)
+}
+
+pub fn convert_hex_string_to_32_byte_array(hex: String) -> Result<Key> {
+    let decoded_hex = hex::decode(hex)?;
+    match decoded_hex.len() {
+        32 => {
+            let mut array = [0; 32];
+            array[..].copy_from_slice(&decoded_hex[..]);
+            Ok(array)
+        }
+        _ => Err(AppError::Custom("✘ Key length invalid!".to_string()))
+    }
 }
 
 #[cfg(test)]
