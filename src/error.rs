fb4b39527bd42b7ea4595c344e65951f9f32972a
Greg Kapka <greg@oraclize.it>
2019-07-13 17:43:51 +0100

    feat(errors): adds more app error `from`s

--- src/error.rs
+++ src/error.rs
@@ -6,18 +6,21 @@ use std::{fmt, option};
 #[derive(Debug)]
 pub enum AppError {
     Custom(String),
+    FmtError(fmt::Error),
     HexError(hex::FromHexError),
     NoneError(option::NoneError),
+    Base58Error(cryptonote_base58::Base58Error),
     ParseBigIntError(num_bigint::ParseBigIntError),
 }
 
-//#[unstable(feature = "try_trait", issue = "42327")]
 impl fmt::Display for AppError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let msg = match *self {
             AppError::Custom(ref msg) => msg.clone(),
             AppError::HexError(ref e) => format!("Hex error: {}", e),
             AppError::NoneError(ref e) => format!("Option error: {:?}", e),
+            AppError::FmtError(ref e) => format!("Formatter error: {}", e),
+            AppError::Base58Error(ref e) => format!("Base58 error: {:?}", e),
             AppError::ParseBigIntError(ref e) => format!("BigUint error: {}", e),
         };
         f.write_fmt(format_args!("{}", msg))
@@ -42,66 +45,15 @@ impl From<option::NoneError> for AppError {
     }
 }
 
-impl From<hex::FromHexError> for AppError {
-    fn from(e: hex::FromHexError) -> AppError {
-        AppError::HexError(e)
-    }
-}
-/*
-
-use hex;
-//use reqwest;
-//use secp256k1;
-use num_bigint;
-use std::error::Error;
-//use std::{io, fmt, num};
-use std::{fmt, option};
-//use sgx_types::sgx_status_t;
-
-#[derive(Debug)]
-pub enum AppError {
-    //Io(io::Error),
-    //Custom(String),
-    //SGXError(sgx_status_t),
-    HexError(hex::FromHexError),
-    //ReqwestError(reqwest::Error),
-    //Secp256k1Error(secp256k1::Error),
-    NoneError(option::NoneError),
-    ParseBigIntError(num_bigint::ParseBigIntError),
-}
-
-//#[unstable(feature = "try_trait", issue = "42327")]
-impl fmt::Display for AppError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let msg = match *self {
-            //AppError::Custom(ref msg) => msg.clone(),
-            //AppError::Io(ref e) => format!("I/O error: {}", e),
-            //AppError::SGXError(ref e) => format!("SGX error: {}", e),
-            AppError::HexError(ref e) => format!("Hex error: {}", e),
-            //AppError::ReqwestError(ref e) => format!("Reqwest error: {}", e),
-            //AppError::Secp256k1Error(ref e) => format!("Crypto error: {}", e),
-            AppError::NoneError(ref e) => format!("Option error: {:?}", e),
-            AppError::ParseBigIntError(ref e) => format!("BigUint error: {}", e),
-        };
-        f.write_fmt(format_args!("{}", msg))
+impl From<fmt::Error> for AppError {
+    fn from(err: fmt::Error) -> AppError {
+        AppError::FmtError(err)
     }
 }
 
-impl Error for AppError {
-    fn description(&self) -> &str {
-        "Program Error"
-    }
-}
-
-impl From<num_bigint::ParseBigIntError> for AppError {
-    fn from(err: num_bigint::ParseBigIntError) -> AppError {
-        AppError::ParseBigIntError(err)
-    }
-}
-
-impl From<option::NoneError> for AppError {
-    fn from(err: option::NoneError) -> AppError {
-        AppError::NoneError(err)
+impl From<cryptonote_base58::Base58Error> for AppError {
+    fn from(err: cryptonote_base58::Base58Error) -> AppError {
+        AppError::Base58Error(err)
     }
 }
 
@@ -110,34 +62,3 @@ impl From<hex::FromHexError> for AppError {
         AppError::HexError(e)
     }
 }
-
-impl Into<String> for AppError {
-    fn into(self) -> String {
-        format!("{}", self)
-    }
-}
-
-impl From<io::Error> for AppError {
-    fn from(err: io::Error) -> AppError {
-        AppError::Io(err)
-    }
-}
-
-impl From<sgx_status_t> for AppError {
-    fn from(err: sgx_status_t) -> AppError {
-        AppError::SGXError(err)
-    }
-}
-
-impl From<secp256k1::Error> for AppError {
-    fn from(e: secp256k1::Error) -> AppError {
-        AppError::Secp256k1Error(e)
-    }
-}
-
-impl From<reqwest::Error> for AppError {
-    fn from(e: reqwest::Error) -> AppError {
-        AppError::ReqwestError(e)
-    }
-}
-*/
